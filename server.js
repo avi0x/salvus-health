6dd3d8dc841e5efa89a5b70dcfdbb37c2780eb44
avi0x <spencer.schneier@protonmail.com>
2018-09-07 14:07:34 -0400

    Setting up Github repo.

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
+const airtable = require('airtable');
+var base = new airtable({apiKey: 'keyShfmLLFP9GB6ck'}).base('appwcbCWvpZ1xES55');
+
+var test = '';
+
+base('BiographicalInformation').find('recyRNmUV7EqwLsKH', function(err, record) {
+    if (err) { console.error(err); return; }
+    test = record['fields']['Patient Name'];
+});
+
 // API calls
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+app.get('/api/goodbye', (req, res) => {
+  res.send({ express: test });
 });
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
